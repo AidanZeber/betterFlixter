d5cbf0c48506a10828bf35c86d38cd1ede8798ad
Aidan Zebertavage <aidanzeber@gmail.com>
2016-03-20 17:34:34 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BetterFlixter::Application.config.secret_key_base = '45a874f0497dcf9f3921c166030dc9333cfc215dececcec88407b2a7c61cdfc640d80b7ab25a6c7044d67aad0c3417aff0aa6e2212f0f864bc8ed3a34b3919f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BetterFlixter::Application.config.session_store :cookie_store, key: '_betterFlixter_session'
